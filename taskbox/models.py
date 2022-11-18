24ae555a16d9963dd2ad59f2f50e107a63416e7a
gregory <gregorysak90@gmail.com>
2022-11-18 15:22:28 -0300

    feat(backend): Tasks model implemented

--- taskbox/models.py
+++ taskbox/models.py
@@ -1,9 +1,18 @@
 from django.db import models
 
-# Create your models here.
-
 class Board(models.Model):
   title = models.CharField(max_length=50)
   
 class Tasks(models.Model):
-  ...
+  statusList = [
+    ("Pending","Pending"),
+    ("Doing","Doing"),
+    ("Done","Done")
+  ]
+  
+  board_id = models.ForeignKey(Board, on_delete=models.CASCADE)
+  create_date = models.DateTimeField(auto_now_add=True)
+  update_date = models.DateTimeField(auto_now=True)
+  title = models.CharField(max_length=50)
+  description = models.TextField()
+  status = models.CharField(max_length=50, choices=statusList)
