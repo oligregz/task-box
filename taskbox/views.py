e47ac245304ec4e6f0aa9981467fe760e8cc3004
gregory <gregorysak90@gmail.com>
2022-11-19 19:56:21 -0300

    feat(backend): views implemented

--- taskbox/views.py
+++ taskbox/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Board, Task
+from .serializer import BoardSerializer, TaskSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+class BoardViewSet(viewsets.ModelViewSet):
+  queryset =  Board.objects.all()
+  serializer_class = BoardSerializer
 
-def index(request):
-  return render(request, "index.html")
+
+class TaskViewSet(viewsets.ModelViewSet):
+  queryset =  Task.objects.all()
+  serializer_class = TaskSerializer
