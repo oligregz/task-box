57f497cde0e2baeb5dad41af185701c6d014f50a
gregory <gregorysak90@gmail.com>
2022-11-19 19:57:09 -0300

    comment

--- taskbox/views.py
+++ taskbox/views.py
@@ -3,6 +3,8 @@ from .models import Board, Task
 from .serializer import BoardSerializer, TaskSerializer
 from rest_framework import viewsets
 
+# as views servem para renderizar a requisição
+
 class BoardViewSet(viewsets.ModelViewSet):
   queryset =  Board.objects.all()
   serializer_class = BoardSerializer
